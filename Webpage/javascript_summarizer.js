608f877de0fcc093914d36d2de703f99b1bcd357
lauwong <lauwong@nuevaschool.org>
2018-04-08 09:17:38 -0700

    Reverted js summarizer to cmnd line

--- Webpage/javascript_summarizer.js
+++ Webpage/javascript_summarizer.js
@@ -1,6 +1,4 @@
-// var prompt = require('prompt');
-//
-
+var prompt = require('prompt');
 var natural = require('natural');
 
 function summarize(text) {
@@ -82,11 +80,11 @@ function summarize(text) {
 // bundler.exclude('wordnet-db');
 // bundler.exclude('lapack');
 
-// prompt.start();
-// prompt.get('paragraph', function(err, result) {
-//   summarize(result.paragraph);
-// });
+prompt.start();
+prompt.get('paragraph', function(err, result) {
+  summarize(result.paragraph);
+});
 
-//summarize(paragraph)
+// summarize(paragraph);
 
 // console.log(summarize("Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend \"Venite Ad Me Omnes\". Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary."));
