9695330105214076861eedf0e8578cbe52c6442b
lauwong <lauwong@nuevaschool.org>
2018-04-07 23:16:45 -0700

    Added prompt

--- Webpage/javascript_summarizer.js
+++ Webpage/javascript_summarizer.js
@@ -1,7 +1,7 @@
-var natural = require('natural');
+var prompt = require('prompt');
 
-var summarize = function(text) {
-
+function summarize(text) {
+  var natural = require('natural');
   stemmer = natural.PorterStemmer;
   stemmer.attach();
 
@@ -36,15 +36,18 @@ var summarize = function(text) {
   //console.log(keys.length);
 
   for(var i = 0; i < sentences.length; i++) {
+    var sentenceLength = 0;
     for(var pair = 0; pair < keys.length; pair++) {
       if(sentences[i].toLowerCase().includes(keys[pair])) {
         if(sentences[i] in sentenceValues) {
           sentenceValues[sentences[i]] += Object.values(freq_table)[pair];
+          sentenceLength++;
         } else {
           sentenceValues[sentences[i]] = Object.values(freq_table)[pair];
         }
       }
     }
+    sentenceValues[sentences[i]] = sentenceValues[sentences[i]]/sentenceLength;
   }
   //console.log(freq_table);
 
@@ -64,14 +67,20 @@ var summarize = function(text) {
 
   var summary = '';
   for(var i = 0; i < sentences.length; i++) {
-    if(sentences[i] in sentenceValues && sentenceValues[sentences[i]] > (1.25 * average)) {
+    if(sentences[i] in sentenceValues && sentenceValues[sentences[i]] > (1.2 * average)) {
       summary += " " + sentences[i];
     }
   }
 
+  console.log("-----------Summary----------\n" + summary);
   return(summary);
 }
 
-module.exports = summarize;
+prompt.start();
+prompt.get('paragraph', function(err, result) {
+  summarize(result.paragraph);
+});
+
+//summarize(paragraph)
 
 // console.log(summarize("Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend \"Venite Ad Me Omnes\". Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary."));
